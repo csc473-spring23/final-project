e4ec1f200668cee1bce3221ad225ee94b0b98465
Sebastian Csar <sacsar@gmail.com>
2023-05-01 19:37:10 -0400

    Define a lint script and correct eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:jsx-a11y-recommended",
-        "prettier"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "jsx-a11y"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["react", "@typescript-eslint", "jsx-a11y"],
+  rules: {},
+};
